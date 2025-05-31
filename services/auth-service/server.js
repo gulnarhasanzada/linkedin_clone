c216b1fc4bf25835d1a8e71f4a1e2e84ad613301
Gulnar Hesenzade <gulnarhesenzade@Gulnars-MacBook-Pro.local>
2025-05-31 15:17:31 -0400

    Switching from Common JS (CJS) to ES Modules (ESM) module system

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -1,24 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connecting to MogoDb Atlas');
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
     app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    });
-})
-.catch(err => {
+        console.log("Server is running on port", process.env.PORT);
+    })
+}).catch((err)=>{
     console.error('MongoDB connection error:', err);
-});
+})
+
 
 
 
