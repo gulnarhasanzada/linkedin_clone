d36d6813f294f55479a405f7eeef16e81dad73a6
Gulnar <gulnar.hesenzade@gmail.com>
2025-06-03 16:25:28 -0400

    Adding vite-federation-plugin for creating microfrontends

--- linkedin-ui/host/vite.config.ts
+++ linkedin-ui/host/vite.config.ts
@@ -2,10 +2,19 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+      react(),
+      tailwindcss(),
+      federation({
+        remotes: {
+          auth: "http://localhost:3001/assets/remoteEntry.ts"
+        }
+      })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
