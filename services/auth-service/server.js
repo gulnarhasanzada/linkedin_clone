967d37b124961f7c5d7ace0fe1cc8fee56ed2250
Gulnar <gulnar.hesenzade@gmail.com>
2025-06-03 22:32:42 -0400

    Implementin cors

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.UI_AUTH_URL,
+        credentials: true
+    })
+)
+
 app.use(express.json());
 app.use("/auth", authRoutes);
 
